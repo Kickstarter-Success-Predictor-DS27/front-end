be3f6a11c74b7eb1370acd43ea0c3fb98aedfb30
mramputatoes <mramputatoes@gmail.com>
2021-07-28 17:38:30 -0600

    more tests

--- pages/predictions.py
+++ pages/predictions.py
@@ -5,7 +5,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
-#import tensorflow as tf
 
 #import numpy as np
 
